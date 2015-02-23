0a266ad97ca1181baa7a47bddced8d3c3720446b
Emily Millard <emilyloumillard@gmail.com>
2015-02-22 18:37:20 -0800

    committing current location. not done work for this sprint but I did include the google maps gemfile which I think will benifit Zach, Wade, and Gurpreet

--- Gemfile
+++ Gemfile
@@ -41,5 +41,12 @@ group :development, :test do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+
+  # for all aspects google maps
+  gem 'gmaps4rails'
+
+  # offical rails geocoder
+  gem 'geocoder'
+
 end
 
---
+++ app/assets/javascripts/googlemaps.js
@@ -0,0 +1,20 @@
+/**
+ * Created by EmilyMillard on 15-02-22.
+ */
+
+
+handler = Gmaps.build('Google');
+handler.buildMap({ internal: {id: 'one_marker'}}, function(){
+    markers = handler.addMarkers([
+        {
+            lat: 0,
+            lng: 0,
+            picture: {
+                url: "https://addons.cdn.mozilla.net/img/uploads/addon_icons/13/13028-64.png",
+                width:  36,
+                height: 36
+            },
+            infowindow: "hello!"
+        }
+    ]);
+});
